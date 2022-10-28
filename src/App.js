64492201086d0d190ecd91bb3aa21827feeeed55
James Drysdale <james.drysdale@codeclan.com>
2022-10-28 21:21:19 +0100

    :sparkles: +PetClass Pets can have a name, species and breed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+const Pet = (props) => {
+    return React.createElement(
+        "div",
+        {},
+        [                                   // Create a div with an array
+            React.createElement("h1", {}, props.name),
+            React.createElement("h2", {}, props.species),
+            React.createElement("h2", {}, props.breed)
+        ]                      
+    )
+}
+
+
 //  Define the content of the app as 'App'
 // This is a function component
 const App = () => {
     return React.createElement(
-        "div",                  // create a simple div
-        {},                     // which has no additional paramaters //P.S. null can be used instead of {}
-        React.createElement(    // whose content is another new element
-            "h1",               // create a h1 inside the div
-            {},                 // with no additional parameters such as class name or id // id: "brand"
-            "Next Best Friend"         // with the content 'Next Best Friend'
-        )
+        "div",                              // create a simple div
+        {},                                 // which has no additional paramaters //P.S. null can be used instead of {}
+        [
+            React.createElement(            // whose content is another new element
+                "h1",                       // create a h1 inside the div
+                {},                         // with no additional parameters such as class name or id // id: "brand"
+                "Next Best Friend"          // with the content 'Next Best Friend'
+            ),
+            React.createElement(Pet, {      //Include the Pet component in the app, define value of props in object
+                name: "Ollie",
+                species: "dog",
+                breed: "Collie"
+            }),       
+            React.createElement(Pet, {
+                name: "Harry",
+                species: "Hamster",
+                breed: "Syrian"
+            }),
+            React.createElement(Pet, {
+                name: "Dexter",
+                species: "Cat",
+                breed: "Ragdoll"
+            })
+        ]
     );
 };
 
