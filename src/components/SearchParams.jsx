454c61528c8ec39224d6b25598569e258d23242d
James Drysdale <james.drysdale@codeclan.com>
2022-10-29 16:25:15 +0100

    :truck: put all components together now app is growing for better organisation

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import useBreedList from "./useBreedList";
-import Results from "./Results";
+import useBreedList from "../useBreedList";
+import Results from "../Results";
 
 const SPECIES = [
   "cat",
