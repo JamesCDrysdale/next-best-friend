71f495d3d54c7ba2b4af8702c89a3052b48ec726
James Drysdale <james.drysdale@codeclan.com>
2022-10-30 19:35:35 +0000

    💄 Theme can now be changed

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -28,7 +28,7 @@ const SearchParams = () => {
   const [breed, setBreed] = useState("");
   const [breeds] = useBreedList(species);
   const [pets, setPets] = useState([]);
-  const [theme] = useContext(ThemeContext);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   useEffect(() => {
     requestPets();
@@ -44,68 +44,88 @@ const SearchParams = () => {
   }
 
   return (
-    <div className="search-params">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          requestPets();
-        }}
-      >
-        <label htmlFor="location">
-          Location
-          <input
-            id="location"
-            value={location}
-            placeholder="Location"
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </label>
-        <label htmlFor="species">
-          Species
-          <select
-            id="species"
-            value={species}
-            onChange={(e) => {
-              setSpecies(e.target.value);
-              setBreed("");
-            }}
-            onBlur={(e) => {
-              setSpecies(e.target.value);
-              setBreed("");
-            }}
-          >
-            <option />
-            {SPECIES.map((species) => (
-              <option key={species} value={species}>
-                {species}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            disabled={!breeds.length}
-            id="breed"
-            value={breed}
-            onChange={(e) => {
-              setBreed(e.target.value);
-            }}
-            onBlur={(e) => {
-              setBreed(e.target.value);
-            }}
-          >
-            <option />
-            {breeds.map((breed) => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
-        <button style={{ backgroundColor: theme }}>Submit</button>
-      </form>
-      <Results pets={pets} />
+    <div>
+      <div className="search-params">
+        <form>
+          <label htmlFor="theme">
+            Theme
+            <select
+              value={theme}
+              onChange={(e) => setTheme(e.target.value)}
+              onBlur={(e) => setTheme(e.target.value)}
+            >
+              <option value="#009ddc">Carolina Blue</option>
+              <option value="#F26430">Satsuma</option>
+              <option value="#2A2D34">Gunmetal</option>
+              <option value="#6761A8">Orchid</option>
+              <option value="#009B72">Green</option>
+            </select>
+          </label>
+        </form>
+      </div>
+      <div className="search-params">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            requestPets();
+          }}
+        >
+          <label htmlFor="location">
+            Location
+            <input
+              id="location"
+              value={location}
+              placeholder="Location"
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </label>
+          <label htmlFor="species">
+            Species
+            <select
+              id="species"
+              value={species}
+              onChange={(e) => {
+                setSpecies(e.target.value);
+                setBreed("");
+              }}
+              onBlur={(e) => {
+                setSpecies(e.target.value);
+                setBreed("");
+              }}
+            >
+              <option />
+              {SPECIES.map((species) => (
+                <option key={species} value={species}>
+                  {species}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="breed">
+            Breed
+            <select
+              disabled={!breeds.length}
+              id="breed"
+              value={breed}
+              onChange={(e) => {
+                setBreed(e.target.value);
+              }}
+              onBlur={(e) => {
+                setBreed(e.target.value);
+              }}
+            >
+              <option />
+              {breeds.map((breed) => (
+                <option key={breed} value={breed}>
+                  {breed}
+                </option>
+              ))}
+            </select>
+          </label>
+          <button style={{ backgroundColor: theme }}>Submit</button>
+        </form>
+        <Results pets={pets} />
+      </div>
     </div>
   );
 };
